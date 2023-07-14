cce91923019569abbd9ca7ed8a48eacaacc5112e
RaghadNaser-1 <126399047+RaghadNaser-1@users.noreply.github.com>
2023-07-14 15:45:30 -0700

    responsive

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -34,7 +34,9 @@
                         </div>
                     </div>
                     <div class="col-md-9">
-                        <div class="row row-cols-3 g-4">
+                        {{-- <div class="row row-cols-3 g-3"> --}}
+                            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+
                             @foreach ($books as $book)
                                 <div class="col mb-4">
                                     <div class="card book-card">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,35 @@
 @include('layouts.header')
 
-@yield('content')
 
-@include('layouts.footer')
+<style>
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    .content {
+        flex: 1;
+    }
+</style>
+
+<div class="wrapper">
+    <div class="content">
+        <!-- Your page content here -->
+        @yield('content')
+
+    </div>
+    {{-- <footer class="bg-light py-5">
+        <div class="container px-4 px-lg-5">
+            <div class="small text-center text-muted">Copyright &copy; 2023 - Library</div>
+        </div>
+    </footer> --}}
+    @include('layouts.footer')
+
+</div>
